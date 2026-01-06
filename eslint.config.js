0ae4bb5f12a39032c8edd8a86fcea7e83769c171
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-06 11:30:45 +0700

    Chore: add eslint rules to encourage yagni and kiss programmer code principle

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,20 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   {
     rules: {
-      
+      "semi": ["error", "never"],
+      "indent": ["error", 4],
+      "eqeqeq": "error",
+      "camelcase": "error",
+      "no-var": "error",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-unassigned-vars": "error",
+      "no-unreachable": "error",
+      "no-unmodified-loop-condition": "error",
+      "no-unreachable-loop": "error",
+      "for-direction": "error",
+      "no-constant-binary-expression": "error",
+      "no-fallthrough": "error",
+      "no-warning-comments": "warn"
     }
   }
 ]);
