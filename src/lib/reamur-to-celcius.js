1c51697ad990c353af179b72801f497c063608e8
askadiliya <askadiliyan@gmail.com>
2026-01-06 13:26:31 +0700

    refactor: menghapus code untuk testing

--- src/lib/reamur-to-celcius.js
+++ src/lib/reamur-to-celcius.js
@@ -6,9 +6,3 @@ function reamurToCelcius(reamur) {
   const celcius = (reamur * 5) / 4;
   return celcius;
 }
-
-
-const suhuReamur = 20;
-const hasil = reamurToCelcius(suhuReamur);
-
-console.log(`${suhuReamur}°Re = ${hasil}°C`);
