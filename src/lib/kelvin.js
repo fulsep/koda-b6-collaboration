305d851adb76aa92cf66849a1018c60da16bf2fb
adityabastyas <adityabastyasmulya@gmail.com>
2026-01-06 17:03:50 +0700

    fix: change nama in kelvin.js

--- src/lib/kelvin.js
+++ src/lib/kelvin.js
@@ -1,6 +1,6 @@
 import { kelvinToCelcius } from "./kelvin-to-celcius.js"
 import { kelvinReamur as kelvinToReamur } from "./kelvin-to-reamur.js"
-import { kelvinToFahrenheit } from "./kelvin-to-fahrenhei.jst"
+import { kelvinToFahrenheit } from "./kelvin-to-fahrenhei.js"
 
 export {kelvinToCelcius, kelvinToReamur, kelvinToFahrenheit}
 
