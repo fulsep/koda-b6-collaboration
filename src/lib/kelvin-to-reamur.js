a855e93d056335134cfd1bc365b86fd65448c584
KODA <vincentius85@gmail.com>
2026-01-06 13:29:26 +0700

    feat: extra feature to do conversion from kelvin to reamur

---
+++ src/lib/kelvin-to-reamur.js
@@ -0,0 +1,12 @@
+export function celciusFarenheit(masukanKelvin){
+    if(typeof masukanKelvin !== "number"){
+        throw new Error("Input harus berupa Number!");
+    }
+    try{
+        const res = (masukanKelvin - 273) * (8/10);
+        return res; 
+    }
+    catch(err){
+        console.log(`Terjadi kesalahan: ${err.message}`);
+    }
+}
