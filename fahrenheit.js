a6682859dbfb0ff3f767c46fb29bc96c2050a372
makarim muhammad <jakamakarim@gmail.com>
2026-01-06 11:01:15 +0700

    feat: add function fahrenheit to reamur

---
+++ fahrenheit.js
@@ -0,0 +1,7 @@
+function fahrenheiToReamurs(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+const fahrenheit = 100;
+const reamurs = fahrenheiToReamurs(fahrenheit);
+console.log('nilai reamur dari fahrenheit adalah', reamurs);
