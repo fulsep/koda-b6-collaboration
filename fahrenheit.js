e5803dbc21da42fb437e903fd85449799da650c5
makarim muhammad <jakamakarim@gmail.com>
2026-01-06 11:53:59 +0700

    delete fahrenheit.js

--- fahrenheit.js
+++
@@ -1,7 +0,0 @@
-function fahrenheiToReamurs(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
-}
-
-const fahrenheit = 100;
-const reamurs = fahrenheiToReamurs(fahrenheit);
-console.log('nilai reamur dari fahrenheit adalah', reamurs);
