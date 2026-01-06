42b4e0d1c5b7db4812bcf2993d0c4c654cd8c2bc
adityabastyas <adityabastyasmulya@gmail.com>
2026-01-06 14:33:39 +0700

    feat: add new file kelvin.js and added import export

---
+++ src/lib/kelvin.js
@@ -0,0 +1,7 @@
+import { kelvinToCelcius } from "./kelvin-to-celcius"
+import { kelvinReamur } from "./kelvin-to-reamur"
+import { kelvinToFahrenheit } from "./kelvin-to-fahrenheit"
+
+export const toCelcius = kelvinToCelcius(10)
+export const toReamur = kelvinReamur(8)
+export const toFahrenheit = kelvinToFahrenheit(9)
